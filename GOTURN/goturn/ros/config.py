6e7d2209a5d8661e43e8c12560e0216bab82069d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-17 11:57:16 +0800

    Delete GOTURN directory

--- GOTURN/goturn/ros/config.py
+++
@@ -1,5 +0,0 @@
-IMAGE_SUB_TOPIC = "camera/tracking/image"
-TRACK_PUB_TOPIC = "track_rect_pub"
-PROTOTXT_PATH = "/home/hddxds/dong/PY-GOTURN/nets/tracker.prototxt"
-MODEL_PATH = "/home/hddxds/dong/PY-GOTURN/nets/tracker.caffemodel"
-GUPID = 0
